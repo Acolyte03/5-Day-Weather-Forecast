7b0ce218596c727eb434cb7ab6fc901444f016d3
Acolyte03 <Acolyte03@gmail.com>
2023-10-25 15:43:27 -0500

    Added starter files and have files referencing each other

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+var weatherUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=" + {lat} + "&lon=" + {lon} + "&exclude=" + {part} + "&appid=6d3d04001d76e5ac03fafd3c5691957f"
+
+
+function weatherSearch(event)
+{
+    event.preventDefault();
+
+}
+weatherSearch();
